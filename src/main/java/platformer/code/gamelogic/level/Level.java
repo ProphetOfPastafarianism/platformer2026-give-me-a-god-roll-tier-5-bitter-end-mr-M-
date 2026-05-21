c166d8c4b3f37f0e7f6b7281d46e8f2d7ac1930e
ProphetOfPastafarianism <magnushuston123@gmail.com>
2026-05-21 15:48:57 +0000

    close to done

--- src/main/java/platformer/code/gamelogic/level/Level.java
+++ src/main/java/platformer/code/gamelogic/level/Level.java
@@ -196,43 +196,73 @@ public class Level {
 	//#############################################################################################################
 	//Your code goes here! 
 	//Please make sure you read the rubric/directions carefully and implement the solution recursively!
-	int i=3;
+	
 	private void water(int col, int row, Map map, int fullness) {
+		int i=fullness;
+		int k=fullness;
 		Water w = new Water(col, row, tileSize, tileset.getImage("Full_water"), this, 3);
 		Water hw = new Water(col, row, tileSize, tileset.getImage("Half_water"), this, 2);
 		Water qw = new Water(col, row, tileSize, tileset.getImage("Quarter_water"), this, 1);
 		Water fw = new Water(col, row, tileSize, tileset.getImage("Falling_water"), this, 0);
+		if(!map.getTiles()[col][row].isSolid()){
 		if(fullness==3){
 		map.addTile(col, row, w);
 		i--;
+		k--;
 		}else if(fullness==2){
 		map.addTile(col, row, hw);	
+		i--;
+		k--;
 		}else if(fullness==1){
 		map.addTile(col, row, qw);	
+		}else{
+		map.addTile(col, row, w);
+		water(col+1, row, map, 1);	
 		}
-	if(!map.getTiles()[col+1][row].isSolid()){
+	}
+	if(!map.getTiles()[col+1][row].isSolid() && !(map.getTiles()[col+1][row] instanceof Water)){
 		if(col+1<map.getTiles().length){
-		if(!map.getTiles()[col+1][row-1].isSolid()){
+			if(map.getTiles()[col][row+1].isSolid()){
 			if(i==2){
 			i--;
 			water(col+1,row,map,2);	
 			}else if(i==1){
 			water(col+1,row,map,1);
 			}
+		}else{
+			if(fullness==2){
+			map.addTile(col+1, row, hw);	
+			
+			}else{
+			map.addTile(col+1, row, qw);
+			}
+			water(col,row+1,map,0);	
+		}
 		}
 	}
-}
-	//if(!map.getTiles()[col-1][row].isSolid()){
-	//	if(col-1>=0){
-		//	if (i>1){
-		//	water(col-1,row,map,2);
-		//	i--;	
-		//	}else if(i==1){
-		//	water(col-1,row,map,1);
-		//	}
-	//	}
-	}
 
+	if(!map.getTiles()[col-1][row].isSolid() && !(map.getTiles()[col-1][row] instanceof Water)){
+		if(col-1>=0){
+			if(map.getTiles()[col-1][row+1].isSolid()){
+			if(k==2){
+			water(col-1,row,map,2);	
+			k--;
+			}else if(k==1){
+			water(col-2,row,map,1);
+			}
+		}else{
+			if(fullness==2){
+			map.addTile(col-1, row, hw);	
+			map.addTile(col-1, row+1, fw);	
+			}else{
+			map.addTile(col-1, row, qw);	
+			map.addTile(col-1, row+1, fw);	
+			}
+			water(col-1,row+1,map,0);	
+		}
+	}
+	}
+}
 
 
 
